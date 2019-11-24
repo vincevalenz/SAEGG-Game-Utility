f1b9007f26ec04268a1034076dc9a69240c1d116
AriEncarnacion <ari.encarnacion.95@gmail.com>
2019-11-23 22:37:10 -0800

    Added GameListActivity -- reusable recycler view activity to be used whenever a list of games needs to be displayed

---
+++ app/src/main/java/com/example/reviewer/GameLibraryActivities/GameListActivity.java
@@ -0,0 +1,14 @@
+package com.example.reviewer.GameLibraryActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_list);
+    }
+}
