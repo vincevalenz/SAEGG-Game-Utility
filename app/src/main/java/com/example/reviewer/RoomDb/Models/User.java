7042c98cf7d2408e749e6696a47051566993ad62
vinnyvalenz <valenzuv@sonoma.edu>
2019-12-02 16:29:38 -0800

    User RoomDb. Still need to store info

---
+++ app/src/main/java/com/example/reviewer/RoomDb/Models/User.java
@@ -0,0 +1,75 @@
+package com.example.reviewer.RoomDb.Models;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "user")
+public class User {
+
+    @PrimaryKey(autoGenerate = true)
+    private int user_id;
+
+    @ColumnInfo(name = "name")
+    private String name;
+
+    @ColumnInfo(name = "unique_id")
+    private String uid;
+
+    @ColumnInfo(name = "email")
+    private String email;
+
+    @ColumnInfo(name = "password")
+    private String password;
+
+    public User(){}
+
+    public User(int user_id, String email, String password, String name, String uid) {
+        this.user_id = user_id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
